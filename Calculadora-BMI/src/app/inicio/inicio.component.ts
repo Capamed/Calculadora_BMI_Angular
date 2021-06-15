239a9c5930f0fa94028ed798edac556f0b36c2b7
Capamed <capamedrp@gmail.com>
2021-06-15 01:17:58 -0500

    BMI calculadora interfaz

--- Calculadora-BMI/src/app/inicio/inicio.component.ts
+++ Calculadora-BMI/src/app/inicio/inicio.component.ts
@@ -7,9 +7,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
+  edad = 35;
+  peso = 60;
+  altura = 170;
+  sexo = '';
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  cambiarAltura(event:any){
+    this.altura = event.target.value;
   }
 
 }
